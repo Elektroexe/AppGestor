25e3345ef4905eca949e5a5d03c637e7e52b243f
Daniel <Daniel@DLARANAVAS>
2017-03-02 17:51:26 +0100

    Entity + Controllers

---
+++ AppGestorWindow/ModelAppGestor/Data/ProgramaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelAppGestor.Data
+{
+    public partial class ProgramaDTO
+    {
+        public int CODIGO { get; set; }
+        public Nullable<int> CODIGOCAT { get; set; }
+        public string NOMBRE { get; set; }
+        public string PLATAFORMA { get; set; }
+        public string VERSION { get; set; }
+        public Nullable<int> DESCARGAS { get; set; }
+        public string HOST { get; set; }
+        public Nullable<bool> INSTALADO { get; set; }
+    }
+}
