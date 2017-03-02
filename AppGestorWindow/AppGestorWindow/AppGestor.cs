d6e8f199dbb88df685095f8e43c22aaf367bb916
Daniel <Daniel@DLARANAVAS>
2017-03-02 18:58:10 +0100

    DATAGRIDVIEW!!!

--- AppGestorWindow/AppGestorWindow/AppGestor.cs
+++ AppGestorWindow/AppGestorWindow/AppGestor.cs
@@ -28,6 +28,51 @@ namespace AppGestorWindow
         {
             programasInstalados = ctlrPrograma.GetAllProgramas();
             dgvAppInstalled.DataSource = programasInstalados;
+            initializeGrid();
+        }
+
+        private void initializeGrid()
+        {
+            
+            foreach (DataGridViewColumn item in dgvAppInstalled.Columns)
+            {
+                switch (item.Name)
+                {
+
+                    case "NOMBRE":
+                        item.Visible = true;
+                        break;
+                    case "DESCARGAS":
+                        item.Visible = true;
+                        item.ReadOnly = true;
+                        break;
+
+                    default:
+                        item.Visible = false;
+                        break;
+
+                }
+            }
+        }
+
+        private void metroGrid1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ProgramaDTO pog = new ProgramaDTO();
+            pog.CODIGO = (int)dgvAppInstalled.Rows[e.RowIndex].Cells["CODIGO"].Value;
+            pog.CODIGOCAT = (int)dgvAppInstalled.Rows[e.RowIndex].Cells["CODIGOCAT"].Value;
+            pog.DESCARGAS = (int)dgvAppInstalled.Rows[e.RowIndex].Cells["DESCARGAS"].Value;
+            pog.HOST = dgvAppInstalled.Rows[e.RowIndex].Cells["HOST"].Value.ToString();
+            pog.NOMBRE = dgvAppInstalled.Rows[e.RowIndex].Cells["NOMBRE"].Value.ToString();
+            pog.PLATAFORMA = dgvAppInstalled.Rows[e.RowIndex].Cells["PLATAFORMA"].Value.ToString();
+            pog.VERSION = dgvAppInstalled.Rows[e.RowIndex].Cells["VERSION"].Value.ToString();
+            pog.INSTALADO = Boolean.Parse(dgvAppInstalled.Rows[e.RowIndex].Cells["INSTALADO"].Value.ToString());
+
+
+
+
+
+            MessageBox.Show(dgvAppInstalled.Rows[e.RowIndex].Cells["CODIGO"].Value.ToString());
         }
     }
 }
+
